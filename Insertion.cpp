e3c59814107ed378a4ffdcb82e0a848aaa16c0e6
Pham Minh Dat <phamminhdat2705@gmail.com>
2025-04-05 05:20:59 +0700

    Basic Sort

--- Insertion.cpp
+++ Insertion.cpp
@@ -2,16 +2,15 @@
 
 void InsertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
-        int key = arr[i]; // Element to be inserted
+        int key = arr[i];
         int j = i - 1;
 
-        // Move elements of arr[0..i-1] that are greater than key
-        // one position ahead of their current position
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
-        arr[j + 1] = key; // Insert the key at the correct position
+        
+        arr[j + 1] = key; 
     }
 }
 
